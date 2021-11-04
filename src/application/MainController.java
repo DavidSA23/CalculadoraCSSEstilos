be340f77494ec832f1462f6c597f86fc99b9c2f5
David <David@10.10.1.102>
2021-11-04 09:37:24 +0100

    Segundo commit y prueba

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -1,5 +1,46 @@
 package application;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 public class MainController {
+	
+	@FXML
+    private Button btnActualizar;
+
+    @FXML
+    private Button btnAñadir;
+
+    @FXML
+    private Button btnBorrar;
+
+    @FXML
+    private TextField tfCodigoEmpleado;
+
+    @FXML
+    private TextField tfNombreEmpleado;
+
+    @FXML
+    private TextField tfPuestoEmpleado;
+
+    @FXML
+    private TableView<?> tvTablaDatosEmpleados;
+
+    @FXML
+    void actualizarEmpleado(ActionEvent event) {
+
+    }
+
+    @FXML
+    void añadirEmpleado(ActionEvent event) {
+
+    }
+
+    @FXML
+    void borrarEmpleado(ActionEvent event) {
+
+    }
+
 	
 }
